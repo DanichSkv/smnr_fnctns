67b89fbdef989f65fd8af0f0b51bcb323feaf1b0
DaniilSkv <danichskv@gmail.com>
2023-10-08 19:09:52 +0500

    Task 29 is complete

---
+++ task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] Create_Array(int length)
+{
+    int[] array = new int [length];
+    Console.WriteLine("Ожидание переменных (разделитель - Enter): ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+}
+
+Console.WriteLine("Привет! Эта программа создаёт одномерный массив! Введи размер массива: ");
+int start_num = Convert.ToInt32(Console.ReadLine());
+
+int[] array_fin = Create_Array(start_num);
+PrintArray(array_fin);
